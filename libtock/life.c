f4025c0fbec244fbb4f2b53fe958bed52bd68c0a
Jason <jasonlu2025@u.northwestern.edu>
2023-12-31 17:46:18 -0600

    Life.c refactor

--- libtock/life.c
+++ libtock/life.c
@@ -1,13 +1,13 @@
-#include "led.h"
+#include "life.h"
 
 int life_meaning(int *life)
 {
-    syscall_return_t rval = command(DRIVER_NUM_LEDS, 0, 0, 0);
+    syscall_return_t rval = command(DRIVER_NUM_LIFE, 0, 0, 0);
     return tock_command_return_u32_to_returncode(rval, (uint32_t *)life);
 }
 
 int define_life(int life_num)
 {
-    syscall_return_t rval = command(DRIVER_NUM_LEDS, life_num, 0, 0);
+    syscall_return_t rval = command(DRIVER_NUM_LIFE, 1, life_num, 0);
     return tock_command_return_novalue_to_returncode(rval);
 }
